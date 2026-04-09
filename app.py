0e3cc9260462dd9d0fd2e6f18afce06fcfba4b17
navya singh <iamnavyasingh2006@gmail.com>
2026-04-09 23:22:59 +0530

    .

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = flask.Flask(__name__)
 @app.route('/')
 def hello_world():
     l=5
-    return 'Hello, World!'
+    return f'Hello, World! The value of l is {l}'
 if __name__ == '__main__':
     app.run(debug=True)
