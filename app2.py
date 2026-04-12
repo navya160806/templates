3871088265a62b26d925a9553fc4a9fb99fad516
navya singh <iamnavyasingh2006@gmail.com>
2026-04-12 23:17:02 +0530

    .

--- app2.py
+++ app2.py
@@ -1,2 +1,9 @@
 from flask  import Flask, render_template
 app = Flask(__name__)
+joblib.dump(new_model, repaired_model_path)
+print(f"Repaired model saved.")
+return {
+"success": True,
+"model_path": repaired_model_path,
+"data_path": clean_data_path,
+"message": "Outliers removed, Bias corrected, Model Retrained."
