b13ea5d9f76562b3a605761ce0045a68aefc7c55
navya singh <iamnavyasingh2006@gmail.com>
2026-04-15 23:06:09 +0530

    .

--- app2.py
+++ app2.py
@@ -20,7 +20,7 @@ except Exception as e:
 @app.route('/')
 def home():
     return render_template('index.html')
-    return "Hello, World!"
+
 if __name__ == '__main__':
     app.run(debug=True)
     
