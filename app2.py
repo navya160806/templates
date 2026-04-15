df3a4b805dcc6126b5ddcca4d5e56ccbf415abcd
navya singh <iamnavyasingh2006@gmail.com>
2026-04-15 23:05:17 +0530

    .

--- app2.py
+++ app2.py
@@ -2,6 +2,7 @@ from flask  import Flask, render_template
 app = Flask(__name__)
 joblib.dump(new_model, repaired_model_path)
 print(f"Repaired model saved.")
+
 return {
     "success": True,
     "model_path": repaired_model_path,
